328082392757341af91e4ec74879f30c28cb8d7c
salkhaleeli <saad@Saads-MBP.hitronhub.home>
2022-09-19 15:03:23 -0600

    adding title

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import Sidebar from './Sidebar';
 
@@ -7,7 +7,7 @@ const Feed = () => {
     <Stack
       sx={{ flexDirection: { sx: 'column', md: 'row' } }}
     >
-      <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }}}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
         <Sidebar />
 
         <Typography
@@ -18,6 +18,21 @@ const Feed = () => {
         </Typography>
       </Box>
 
+      <Box
+        p={2}
+        sx={{ overflowY: 'auto', height: '90vh', flex: 2}}
+      >
+        <Typography
+          variant='h4'
+          fontWeight='bold' mb={2}
+          sx={{ color: 'white' }}
+        >
+          New <span style={{ color: '#F31503' }}>
+            Videos
+          </span>
+        </Typography>
+
+      </Box>
     </Stack>
   )
 }
