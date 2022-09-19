36028b564e5589b2824713a3a43ceaed65ddab01
salkhaleeli <saad@Saads-MBP.hitronhub.home>
2022-09-19 14:54:49 -0600

    fixing sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,7 +23,7 @@ const Sidebar = () => {
           <span style={{color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>
             {category.icon}
           </span>
-          <span style={{ opacity}}>
+          <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8'}}>
             {category.name}
           </span>
         </button>
