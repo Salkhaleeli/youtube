e4eb3fb099a4b93c14f5152007c03011936df7aa
salkhaleeli <saad@Saads-MBP.hitronhub.home>
2022-09-20 11:55:46 -0600

    indent

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -6,28 +6,29 @@ import { demoProfilePicture } from "../utils/constants";
 const ChannelCard = ({ channelDetail }) => {
   return (
     <Box
-    sx={{
-      boxShadow: 'none',
-      borderRadius: '20px',
-    }}
+      sx={{
+        boxShadow: 'none',
+        borderRadius: '20px',
+      }}
     >
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
         <CardContent
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          textAlign: 'center',
-          color: '#fff'}}>
-            <CardMedia
-              image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
-              alt={channelDetail?.snippet?.title}
-              sx={{borderRadius: '50%', height: '180px', width: '180px', mb: 2,  border: '1px solid #e3e3e3'}}
-            />
-            <Typography variant="h6">
-              {channelDetail?.snippet?.title}
-              <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
-            </Typography>
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            textAlign: 'center',
+            color: '#fff'
+          }}>
+          <CardMedia
+            image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+            alt={channelDetail?.snippet?.title}
+            sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}
+          />
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
+          </Typography>
 
         </CardContent>
 
