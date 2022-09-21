91e7794d9ff0c90e88ca444a28e44b5b0007ce8e
salkhaleeli <saad@Saads-MBP.hitronhub.home>
2022-09-21 10:52:53 -0600

    fixing channelCard alignment

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -9,6 +9,12 @@ const ChannelCard = ({ channelDetail }) => {
       sx={{
         boxShadow: 'none',
         borderRadius: '20px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: { xs: '356px', md: '320px'},
+        height: '326px',
+        margin: 'auto'
       }}
     >
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
@@ -29,6 +35,11 @@ const ChannelCard = ({ channelDetail }) => {
             {channelDetail?.snippet?.title}
             <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
           </Typography>
+          {channelDetail?.statistics?.subscriberCount && (
+            <Typography>
+              {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+            </Typography>
+          )}
 
         </CardContent>
 
