c44aec8b66bb0ef3f011418f94db6e84d45100a2
salkhaleeli <saad@Saads-MBP.hitronhub.home>
2022-09-26 11:11:04 -0600

    adding show videos feat and arranging videos page

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,7 +2,8 @@ import { Stack, Box } from "@mui/material";
 import {VideoCard, ChannelCard} from "./";
 
 const Videos = ({ videos }) => {
-  console.log("videos", videos);
+
+  if(!videos?.length) return 'Loading...';
   return (
     <Stack
       direction='row'
